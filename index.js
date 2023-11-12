ea0e241b72ed87887d8e3de36f32d7752aa1add3
lhgl <luizhleite@outlook.com>
2023-11-12 20:19:19 -0300

    Add init files

---
+++ index.js
@@ -0,0 +1,13 @@
+// New
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
+
+// New
+const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "user", "content": "Hello!"}],
+  });
+  console.log(chatCompletion.choices[0].message);
